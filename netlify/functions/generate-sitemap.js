d70c4e9cab505598bb9218ea1508fee0bccd38ec
Tyl3r035 <tylerpaulson888@gmail.com>
2024-06-11 15:34:01 -0500

    Sitemap

--- netlify/functions/generate-sitemap.js
+++ netlify/functions/generate-sitemap.js
@@ -1,46 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
-const baseUrl = 'https://www.monvvo.com';
-const directoryPath = path.join(__dirname, 'public');
-
-function generateSitemap() {
-    let urls = [];
-
-    function readDirectory(directory) {
-        fs.readdirSync(directory).forEach(file => {
-            const fullPath = path.join(directory, file);
-            if (fs.lstatSync(fullPath).isDirectory()) {
-                readDirectory(fullPath);
-            } else {
-                const relativePath = fullPath.replace(directoryPath, '').replace(/\\/g, '/');
-                if (relativePath.endsWith('.html')) {
-                    urls.push({
-                        loc: `${baseUrl}${relativePath}`,
-                        lastmod: new Date().toISOString().split('T')[0],
-                        changefreq: 'monthly',
-                        priority: 0.8
-                    });
-                }
-            }
-        });
+exports.handler = async function(event, context) {
+    const baseUrl = 'https://www.monvvo.com';
+    const metadataPath = path.join(__dirname, 'pages-metadata.json');
+    
+    // Read the JSON file
+    let pagesMetadata;
+    try {
+        pagesMetadata = JSON.parse(fs.readFileSync(metadataPath, 'utf8'));
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Error reading pages metadata'
+        };
     }
 
-    readDirectory(directoryPath);
-
+    // Generate sitemap content
     let sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
-    urls.forEach(url => {
+    pagesMetadata.forEach(page => {
         sitemapContent += `  <url>\n`;
-        sitemapContent += `    <loc>${url.loc}</loc>\n`;
-        sitemapContent += `    <lastmod>${url.lastmod}</lastmod>\n`;
-        sitemapContent += `    <changefreq>${url.changefreq}</changefreq>\n`;
-        sitemapContent += `    <priority>${url.priority}</priority>\n`;
+        sitemapContent += `    <loc>${page.loc}</loc>\n`;
+        sitemapContent += `    <lastmod>${page.lastmod}</lastmod>\n`;
+        sitemapContent += `    <changefreq>${page.changefreq}</changefreq>\n`;
+        sitemapContent += `    <priority>${page.priority}</priority>\n`;
         sitemapContent += `  </url>\n`;
     });
     sitemapContent += `</urlset>`;
 
-    fs.writeFileSync(path.join(directoryPath, 'sitemap.xml'), sitemapContent);
-    console.log('Sitemap generated and saved to public/sitemap.xml');
-}
-
-generateSitemap();
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+        body: sitemapContent,
+    };
+};
