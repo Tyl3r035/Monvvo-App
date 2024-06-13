ddd3a7ba2ef379fe36fb9c30525fb2d6056aef06
Tyl3r035 <tylerpaulson888@gmail.com>
2024-06-13 17:02:19 -0500

    Webpack.Config.js
    Added functionality for css on other pages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,3 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-// module.exports = {
-//     entry: './public/js/index.js',
-//     output: {
-//         filename: '[name].[contenthash].js',
-//         path: path.resolve(__dirname, 'dist'),
-//         publicPath: '/'
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.css$/,
-//                 use: [MiniCssExtractPlugin.loader, 'css-loader']
-//             },
-//             {
-//                 test: /\.js$/,
-//                 exclude: /node_modules/,
-//                 use: {
-//                     loader: 'babel-loader',
-//                     options: {
-//                         presets: ['@babel/preset-env']
-//                     }
-//                 }
-//             }
-//         ]
-//     },
-//     plugins: [
-//         new CleanWebpackPlugin(),
-//         new HtmlWebpackPlugin({
-//             template: './public/index.html',
-//             filename: 'index.html'
-//         }),
-//         new MiniCssExtractPlugin({
-//             filename: '[name].[contenthash].css'
-//         })
-//     ],
-//     devServer: {
-//         static: {
-//             directory: path.join(__dirname, 'dist')
-//         },
-//         compress: true,
-//         port: 9000,
-//         open: true,
-//         historyApiFallback: true,
-//         watchFiles: ['public/**/*']
-//     },
-//     stats: {
-//         children: true
-//     },
-//     mode: 'development'
-// };
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -96,7 +41,9 @@ module.exports = {
         }),
         new CopyWebpackPlugin({
             patterns: [
-                { from: 'public/img', to: 'img' }
+                { from: 'public/img', to: 'img' },
+                { from: 'public/css', to: 'css' },  // Copy CSS files
+                { from: 'public/Knowledge-Center', to: 'Knowledge-Center' } // Copy Knowledge-Center folder
             ]
         })
     ],
