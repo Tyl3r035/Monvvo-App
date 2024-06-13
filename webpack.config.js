12e273285e605d048526c06cb3cfa1a22b1f6152
Tyl3r035 <tylerpaulson888@gmail.com>
2024-06-13 16:09:58 -0500

    Update to fix broken logo

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,64 @@
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// module.exports = {
+//     entry: './public/js/index.js',
+//     output: {
+//         filename: '[name].[contenthash].js',
+//         path: path.resolve(__dirname, 'dist'),
+//         publicPath: '/'
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.css$/,
+//                 use: [MiniCssExtractPlugin.loader, 'css-loader']
+//             },
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 use: {
+//                     loader: 'babel-loader',
+//                     options: {
+//                         presets: ['@babel/preset-env']
+//                     }
+//                 }
+//             }
+//         ]
+//     },
+//     plugins: [
+//         new CleanWebpackPlugin(),
+//         new HtmlWebpackPlugin({
+//             template: './public/index.html',
+//             filename: 'index.html'
+//         }),
+//         new MiniCssExtractPlugin({
+//             filename: '[name].[contenthash].css'
+//         })
+//     ],
+//     devServer: {
+//         static: {
+//             directory: path.join(__dirname, 'dist')
+//         },
+//         compress: true,
+//         port: 9000,
+//         open: true,
+//         historyApiFallback: true,
+//         watchFiles: ['public/**/*']
+//     },
+//     stats: {
+//         children: true
+//     },
+//     mode: 'development'
+// };
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './public/js/index.js',
@@ -36,6 +93,11 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash].css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'public/img', to: 'img' }
+            ]
         })
     ],
     devServer: {
