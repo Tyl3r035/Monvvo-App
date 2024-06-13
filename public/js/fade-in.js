a958e2de63ca144f4ae7a4641cbd35e8800971db
Tyl3r035 <tylerpaulson888@gmail.com>
2024-06-12 21:09:30 -0500

    Get rid of fade-in.js to improve FCP for page speed.

--- public/js/fade-in.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add fade-in class to the body
-    document.body.classList.add('fade-in');
-
-    // Make the body visible after a short delay to allow the fade-in effect
-    setTimeout(function() {
-        document.body.classList.add('visible');
-    }, 10);
-});
