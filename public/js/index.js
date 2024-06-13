eacf25141fe4719d3dba1ec5f802073aab0bf7d7
Tyl3r035 <tylerpaulson888@gmail.com>
2024-06-13 15:46:19 -0500

    Changes to Webpack

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
+// index.js
 import '../css/reset.css';
 import '../css/styles.css';
 import './main.js';
 import './utils.js';
-
 // Your additional JavaScript logic here
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = (env, argv) => {
-    const isProduction = argv.mode === 'production';
-
-    return {
-        entry: './public/js/index.js',
-        output: {
-            filename: isProduction ? 'main.[contenthash].js' : 'main.js',
-            path: path.resolve(__dirname, 'dist'),
-            publicPath: '/',
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: [
-                        isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
-                        'css-loader'
-                    ],
-                },
-                {
-                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                    type: 'asset/resource',
-                    generator: {
-                        filename: 'img/[name][ext]'
-                    },
-                },
-            ],
-        },
-        plugins: [
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: './public/index.html',
-                filename: 'index.html',
-            }),
-            ...(isProduction ? [new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })] : []),
-        ],
-        devServer: {
-            static: {
-                directory: path.join(__dirname, 'public'),
-                publicPath: '/',
+module.exports = {
+    entry: './public/js/index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
-            compress: true,
-            port: 9000,
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
         },
-        devtool: isProduction ? 'source-map' : 'eval-source-map',
-    };
+        compress: true,
+        port: 9000,
+        open: true,
+        historyApiFallback: true,
+        watchFiles: ['public/**/*']
+    },
+    stats: {
+        children: true
+    },
+    mode: 'development'
 };
