54555cb3c02a798cc4f5d87c564a29fe100a250f
Tyl3r035 <tylerpaulson888@gmail.com>
2024-06-23 23:38:16 -0500

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -4,70 +4,22 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const pages = [
-    {
-        template: './public/index.html',
-        filename: 'index.html',
-    },
-    {
-        template: './public/404.html',
-        filename: '404.html',
-    },
-    {
-        template: './public/privacy-policy.html',
-        filename: 'privacy-policy.html',
-    },
-    {
-        template: './public/monvvo-disclaimer.html',
-        filename: 'monvvo-disclaimer.html',
-    },
-    {
-        template: './public/knowledge-center/how-to-calculate-mortgage-blog.html',
-        filename: 'knowledge-center/how-to-calculate-mortgage-blog.html',
-    },
-    {
-        template: './public/knowledge-center/mortgage-calculator-make-informed-home-loan-decisions.html',
-        filename: 'knowledge-center/mortgage-calculator-make-informed-home-loan-decisions.html',
-    },
-    {
-        template: './public/mortgage/down-payment-calculator.html',
-        filename: 'mortgage/down-payment-calculator.html',
-    },
-    {
-        template: './public/mortgage/home-equity-calculator.html',
-        filename: 'mortgage/home-equity-calculator.html',
-    },
-    {
-        template: './public/mortgage/mortgage-payoff-calculator.html',
-        filename: 'mortgage/mortgage-payoff-calculator.html',
-    },
-    {
-        template: './public/loan/auto-loan-calculator.html',
-        filename: 'loan/auto-loan-calculator.html',
-    },
-    {
-        template: './public/loan/loan-amortization-calculator.html',
-        filename: 'loan/loan-amortization-calculator.html',
-    },
-    {
-        template: './public/loan/personal-loan-calculator.html',
-        filename: 'loan/personal-loan-calculator.html',
-    },
-    {
-        template: './public/loan/student-loan-calculator.html',
-        filename: 'loan/student-loan-calculator.html',
-    },
-    {
-        template: './public/investment/retirement-savings-calculator.html',
-        filename: 'investment/retirement-savings-calculator.html',
-    },
-    {
-        template: './public/investment/investment-growth-calculator.html',
-        filename: 'investment/investment-growth-calculator.html',
-    },
-    {
-        template: './public/investment/compound-interest-calculator.html',
-        filename: 'investment/compound-interest-calculator.html',
-    }
+    { template: './public/index.html', filename: 'index.html' },
+    { template: './public/404.html', filename: '404.html' },
+    { template: './public/privacy-policy.html', filename: 'privacy-policy.html' },
+    { template: './public/monvvo-disclaimer.html', filename: 'monvvo-disclaimer.html' },
+    { template: './public/knowledge-center/how-to-calculate-mortgage-blog.html', filename: 'knowledge-center/how-to-calculate-mortgage-blog.html' },
+    { template: './public/knowledge-center/mortgage-calculator-make-informed-home-loan-decisions.html', filename: 'knowledge-center/mortgage-calculator-make-informed-home-loan-decisions.html' },
+    { template: './public/mortgage/down-payment-calculator.html', filename: 'mortgage/down-payment-calculator.html' },
+    { template: './public/mortgage/home-equity-calculator.html', filename: 'mortgage/home-equity-calculator.html' },
+    { template: './public/mortgage/mortgage-payoff-calculator.html', filename: 'mortgage/mortgage-payoff-calculator.html' },
+    { template: './public/loan/auto-loan-calculator.html', filename: 'loan/auto-loan-calculator.html' },
+    { template: './public/loan/loan-amortization-calculator.html', filename: 'loan/loan-amortization-calculator.html' },
+    { template: './public/loan/personal-loan-calculator.html', filename: 'loan/personal-loan-calculator.html' },
+    { template: './public/loan/student-loan-calculator.html', filename: 'loan/student-loan-calculator.html' },
+    { template: './public/investment/retirement-savings-calculator.html', filename: 'investment/retirement-savings-calculator.html' },
+    { template: './public/investment/investment-growth-calculator.html', filename: 'investment/investment-growth-calculator.html' },
+    { template: './public/investment/compound-interest-calculator.html', filename: 'investment/compound-interest-calculator.html' }
 ];
 
 module.exports = {
