5d483984107868aa696ce275d2a2eb359e4baf7a
Tyl3r035 <tylerpaulson888@gmail.com>
2024-12-24 01:38:44 -0600

    Initial PMI Page added

---
+++ dist/393.21420d6da3399bc53382.js
@@ -0,0 +1,16 @@
+"use strict";
+(self["webpackChunkmonvvo_app"] = self["webpackChunkmonvvo_app"] || []).push([[393],{
+
+/***/ 393:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "images/down-arrow-white.d5b9630eb81a1b262813dc626c5c4e49.svg");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=393.21420d6da3399bc53382.js.map
